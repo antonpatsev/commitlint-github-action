4744bd8d3909dd9915270f3d7175479c86d6c740
Anton Patsev <a.patsev@sxope.com>
2022-06-08 21:18:52 +0600

    feat(ggg): Add scope-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional', "@commitlint/parse"],
+  rules: {
+    "scope-enum": [2, "always", [
+      "cicd",
+      "git",
+      "github",
+      "gke",
+      "iam",
+      "tf"
+    ]],
+  },
   parserOpts: {
     headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
     headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
