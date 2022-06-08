ae1f65efb10934d255d540b111b6251ff2d117c6
Anton Patsev <a.patsev@sxope.com>
2022-06-08 21:52:27 +0600

    fix(gke): add subject-empty

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   extends: ['@commitlint/config-conventional', "@commitlint/parse"],
-//  rules: {
-//    "scope-enum": [2, "always", [
-//      "cicd",
-//      "git",
-//      "github",
-//      "gke",
-//      "iam",
-//      "tf"
-//    ]],
-//  },
+  rules: {
+    "subject-empty": [2, "never"],
+    "scope-enum": [2, "always", [
+      "cicd",
+      "git",
+      "github",
+      "gke",
+      "iam",
+      "tf"
+    ]]
+  },
   parserPreset: {
     parserOpts: {
       headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
