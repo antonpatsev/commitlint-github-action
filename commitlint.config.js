9afa0141521e8dfb3e075228fe949a0cdc3a82bb
Anton Patsev <a.patsev@sxope.com>
2022-06-08 21:59:16 +0600

    fix(gke): disable headerCorrespondence

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,8 +14,8 @@ module.exports = {
   parserPreset: {
     parserOpts: {
       headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
-      // headerCorrespondence: ['type', 'scope', 'ticket', 'subject']
-      headerCorrespondence: ['subject']
+//      headerCorrespondence: ['type', 'scope', 'ticket', 'subject']
+//      headerCorrespondence: ['subject']
       },
     },
 };
