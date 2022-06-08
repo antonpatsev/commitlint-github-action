8392fe4765e15ed91236d5ef3f0b7391702d3477
Anton Patsev <a.patsev@sxope.com>
2022-06-08 22:02:00 +0600

    fix(gke): 'subject-empty': [0],

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,6 +11,18 @@ module.exports = {
 //      "tf"
 //    ]]
 //  },
+  rules: {
+    'body-leading-blank': [0],
+    'footer-leading-blank': [0],
+    'header-max-length': [0],
+    'scope-case': [0],
+    'subject-case': [0],
+    'subject-empty': [0],
+    'subject-full-stop': [0],
+    'type-case': [0],
+    'type-empty': [0],
+    'type-enum': [0],
+  },
   parserPreset: {
     parserOpts: {
       headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
