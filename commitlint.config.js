a05c885c37650d60d1381e91d320443aed287ef6
Anton Patsev <a.patsev@sxope.com>
2022-06-08 20:50:41 +0600

    change headerPattern

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   parserOpts: {
-    headerPattern: /\(DEVOPS[^)]*\)$/,
+    headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
     headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
   },
 };
