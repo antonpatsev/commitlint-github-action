bc38f1af7ee3c1b4ebb73ecae60cce57b925795d
Anton Patsev <a.patsev@sxope.com>
2022-06-08 21:44:35 +0600

    fix(gke): disable rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   extends: ['@commitlint/config-conventional', "@commitlint/parse"],
-  rules: {
-    "scope-enum": [2, "always", [
-      "cicd",
-      "git",
-      "github",
-      "gke",
-      "iam",
-      "tf"
-    ]],
-  },
+//  rules: {
+//    "scope-enum": [2, "always", [
+//      "cicd",
+//      "git",
+//      "github",
+//      "gke",
+//      "iam",
+//      "tf"
+//    ]],
+//  },
   parserPreset: {
-  parserOpts: {
-    headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
-    headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
+    parserOpts: {
+      headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
+      headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
+      },
     },
-  },
 };
