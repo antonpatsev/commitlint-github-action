b12d2ada7c7958c0ff4a65340fcb585654765b6c
Anton Patsev <a.patsev@sxope.com>
2022-06-08 21:56:37 +0600

    fix(gke): headerCorrespondence: ['subject']

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,21 @@
 module.exports = {
   extends: ['@commitlint/config-conventional', "@commitlint/parse"],
-  rules: {
-    "subject-empty": [2, "never"],
-    "scope-enum": [2, "always", [
-      "cicd",
-      "git",
-      "github",
-      "gke",
-      "iam",
-      "tf"
-    ]]
-  },
+//  rules: {
+//    "subject-empty": [2, "never"],
+//    "scope-enum": [2, "always", [
+//      "cicd",
+//      "git",
+//      "github",
+//      "gke",
+//      "iam",
+//      "tf"
+//    ]]
+//  },
   parserPreset: {
     parserOpts: {
       headerPattern: /^(feat|fix)\((cicd|git|github|gke|iam|tf)\):\s.*(DEVOPS-[0-9]+)/,
-      headerCorrespondence: ['type', 'scope', 'ticket', 'subject'],
+      // headerCorrespondence: ['type', 'scope', 'ticket', 'subject']
+      headerCorrespondence: ['subject']
       },
     },
 };
